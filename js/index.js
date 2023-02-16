f05d1ef11623cbe6e25a7d1030d45a16bc423934
JuanferGG <juanfe15777@gmailcom>
2023-02-16 11:03:30 -0500

    version 3 curriculum

--- js/index.js
+++ js/index.js
@@ -1,19 +1,15 @@
-
-// const miTexto = document.getElementById("textoEjemplo");
-// miTexto.style.color = "red";
-
-// function cambiarBlue(){
-//     miTexto.style.color = "blue"
-// }
-
-// function cambiarRed(){
-//     miTexto.style.color = "red"
-// }
-
-window.onclick = myFunction;
-
-function myFunction() {
-    document.getElementsByTagName("BODY")[0].style.backgroundColor = "rgb(77, 71, 71)";
+function changeState(state){
+    if (state == true){
+        return false
+    } else if(state == false) {
+        return true
+    }
 }
 
-// background: rgb(207, 196, 196);
+let estado = new Boolean(true)
+
+const body = document.querySelector('body')
+
+function darkMode(){
+    console.log(changeState(estado))
+}
